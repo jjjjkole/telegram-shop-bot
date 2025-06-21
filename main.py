e4acb4bb07b648d59f0daa4635daf6d470b33130
jjjjkole <119084508+jjjjkole@users.noreply.github.com>
2025-06-21 17:55:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 import os
 from datetime import datetime
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("7796439721:AAGDOAFhAy3km1z7wyn7OHwGr6wlnxgxC_0")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
